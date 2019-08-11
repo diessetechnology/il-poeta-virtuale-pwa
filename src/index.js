51b91e29ffca676a1be86df69e0f46756520db81
Davide Salamino <davide.salamino@diessetechnology.it>
2019-08-12 01:39:07 +0200

    Aggiunte le poesie e aggiustato il CSS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Input } from "semantic-ui-react";
+import { Input, Button } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Inserisci il nome di una poesia...e il tuo computer te la pronuncerà!
-      </h1>
-      <Input size="big" style={{ width: "100vw" }} />
+      <div className="mainapp">
+        <div className="maincontainer">
+          <h1>
+            Inserisci il nome di una poesia...e il tuo computer te la
+            pronuncerà!
+          </h1>
+          <Input
+            size="big"
+            style={{
+              width: "100%"
+            }}
+          />
+          <Button
+            style={{
+              width: "100%"
+            }}
+          >
+            Recita questa poesia!
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
